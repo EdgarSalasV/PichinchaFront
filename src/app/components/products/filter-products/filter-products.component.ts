c6bf5d450a4282dc52ac372968650c7ece686916
EdgarSalasV <edgardre01@gmail.com>
2023-08-22 11:58:52 -0600

    Added filter input funtionalities && result limit select

--- src/app/components/products/filter-products/filter-products.component.ts
+++ src/app/components/products/filter-products/filter-products.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs/internal/Subject';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { debounceTime, delay, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { ApiService } from 'src/app/services/api.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-filter-products',
@@ -11,33 +12,10 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./filter-products.component.css'],
 })
 export class FilterProductsComponent {
-  private readonly searchSubject = new Subject<string>();
-  private searchSubscription?: Subscription;
-
-  constructor(private searchService: ApiService) {}
-
-  public ngOnInit(): void {
-  //   this.searchSubscription = this.searchSubject
-  //     .pipe(
-  //       debounceTime(300),
-  //       distinctUntilChanged(),
-  //       switchMap((searchQuery) => {
-  //         if (searchQuery === undefined || searchQuery || searchQuery.length === 0) {
-  //           return of([]);
-  //         }
-      
-  //         const result = this.productList?.filter(({ name, description }) =>
-  //           `${name} ${description}`.toLowerCase().includes(searchQuery.toLowerCase())
-  //         );
-      
-  //         return of(result).pipe(delay(1000));
-  //       }
-  //     )
-  //     .subscribe((results) => (this.searchResults = results));
-  }
+  constructor(private productService: ProductService) {}
 
   public onSearchQueryInput(event: Event): void {
     const searchQuery = (event.target as HTMLInputElement).value;
-    this.searchSubject.next(searchQuery?.trim());
+    this.productService.filterSubject.next(searchQuery?.trim());
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,27 +1,56 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
-import { catchError } from 'rxjs';
+import { Subject, catchError, debounce, debounceTime } from 'rxjs';
 import { iProduct } from '../interfaces/Product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
+  public totalProducts: iProduct[] = [];
   public products: iProduct[] = [];
   public limit = 5;
 
-  constructor(private apiService: ApiService) {}
+  public filterSubject = new Subject();
+
+  constructor(private apiService: ApiService) {
+    this.filterSubject
+      .pipe(debounceTime(500))
+      .subscribe((value) => {
+        console.log('Debounce 500 ms executing ...')
+        this.filterProducts(value as string);
+      })
+  }
 
   public getProducts(): void {
     this.apiService.getAll()
       .pipe(catchError(this.apiService.handleError))
       .subscribe(res => {
-        this.products = res.splice(0, this.limit);
+        this.totalProducts = res;
+        this.products = this.getProductsByLimit();
       });
+  }
+
+  public getProductsByLimit() {
+    return this.totalProducts.slice(0, this.limit);
   }
 
   public changeLimit(value: number): void {
     this.limit = value;
+
     this.getProducts();
+  }
+
+  public filterProducts(value: string): void {
+    if (value === '') {
+      this.products = this.getProductsByLimit();
+      return;
+    }
+
+    const productsToFilter = this.getProductsByLimit();
+
+    this.products = productsToFilter.filter(({ name }) => {
+      return name.includes(value);
+    });
   }
 }
